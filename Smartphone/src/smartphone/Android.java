d658fb4a103966d14cd77f3a1f759168f43ac640
elladwipujanaasyani <112689317+elladwipujanaasyani@users.noreply.github.com>
2022-11-07 15:45:12 +0700

    Add files via upload

---
+++ Smartphone/src/smartphone/Android.java
@@ -0,0 +1,29 @@
+// created by 21343022_Ella Dwipujana Asyani
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Other/File.java to edit this template
+ */
+package smartphone;
+
+/**
+ *
+ * @author Ella Dwipujana Asyani
+ */
+public class Android {
+        String merk, warna;
+    
+    public void nyala(){
+        System.out.println("Android menyala");
+    }
+
+    public void panggilan(){
+        System.out.println("Kring Kring Kring, ada panggilan masuk");
+    }
+    
+    public void sms(){
+        System.out.println("Tenenoeeett, ada SMS baru");
+    }
+    
+    public void shutdown(){
+        System.out.println("Android dimatikan");
+    }}
---
+++ Smartphone/src/smartphone/Smartphone.java
@@ -0,0 +1,21 @@
+// created by 21343022_Ella Dwipujana Asyani
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package smartphone;
+
+/**
+ *
+ * @author Ella Dwipujana Asyani
+ */
+public class Smartphone {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
